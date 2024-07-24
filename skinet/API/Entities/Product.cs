9e32ecc43b7f65f1009f75338f044c7c277335f4
Nurarat Sangreuang <nurarat0024@gmail.com>
2024-07-24 15:28:24 +0700

    latest

--- skinet/API/Entities/Product.cs
+++ skinet/API/Entities/Product.cs
@@ -1,8 +1,13 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities
 {
     public class Product
     {
-        public int MyProperty { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
     }
 }
--- skinet/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ skinet/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40112ee427ae68b449274c0ce35e30fb45178a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
